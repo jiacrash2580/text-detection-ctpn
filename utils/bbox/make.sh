f18f91ba24d941b3f76104adc00fc40ac56722bd
jiacrash2580 <jiacrash@sina.com>
2019-04-30 18:25:54 +0800

    提交了基于python3和cpu进行vvbox组件编译

--- utils/bbox/make.sh
+++ utils/bbox/make.sh
@@ -1,3 +1,7 @@
-python setup.py install
-mv build/*/*.so ./
-rm -rf build/
+#!/usr/bin/env bash
+cython -3 bbox.pyx
+cython -3 nms.pyx
+python3 setup.py build_ext --inplace
+mv bbox*.so bbox.so
+mv nms*.so nms.so
+rm -rf build
